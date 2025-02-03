94dcaa0e5736da0da51bafc33b391d53b66ab491
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2025-02-03 13:22:17 +0330

    Initial of qml UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include "mainwindow.h"
+#include "chatsession.h"
+#include "chatsmodel.h"
+#include "settingsdialog.h"
+#include "modelmanager.h"
 
 #include <QApplication>
 #include <QStyleFactory>
+
+#ifdef QT_QUICKCONTROLS2_LIB
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QDateTime>
+#include <QQuickStyle>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -12,13 +23,43 @@ int main(int argc, char *argv[])
     app.setOrganizationName("Aseman");
     app.setWindowIcon(QIcon(":/ui/icons/icon.svg"));
 
-    // qDebug() << QStyleFactory::keys();
-    // auto style = QStyleFactory::create("Adwaita-Dark");
-    // if (style)
-    //     app.setStyle(style);
+#ifdef QT_QUICKCONTROLS2_LIB
+    if (!app.arguments().contains("--widgets"))
+    {
+        QScopedPointer<ChatsModel> chatsModel(new ChatsModel);
+        auto session = new ChatSession(chatsModel.get(), chatsModel.get());
 
-    MainWindow win;
-    win.show();
+        qmlRegisterSingletonInstance("QLLM", 1, 0, "ChatsModel", chatsModel.get());
+        qmlRegisterSingletonInstance("QLLM", 1, 0, "ChatSession", session);
+        qmlRegisterType<ModelManager>("QLLM", 1, 0, "ModelManager");
 
-    return app.exec();
+        const auto desktop = qgetenv("DESKTOP_SESSION");
+        const auto stylesList = QQuickStyle::availableStyles();
+        const QSet<QString> styles(stylesList.begin(), stylesList.end());
+        if (styles.contains("org.kde.desktop") && desktop == "plasma")
+            QQuickStyle::setStyle("org.kde.desktop");
+        else if (styles.contains("Plasma") && desktop == "plasma")
+            QQuickStyle::setStyle("Plasma");
+        else if (styles.contains("Suru"))
+            QQuickStyle::setStyle("Suru");
+        else
+            QQuickStyle::setStyle("Default");
+
+        QQmlApplicationEngine engine;
+        engine.load(QUrl("qrc:/qml/main.qml"));
+
+        return app.exec();
+    }
+    else
+#endif
+    {
+        // qDebug() << QStyleFactory::keys();
+        // auto style = QStyleFactory::create("Adwaita-Dark");
+        // if (style)
+        //     app.setStyle(style);
+        MainWindow win;
+        win.show();
+
+        return app.exec();
+    }
 }
---
+++ src/qml/main.qml
@@ -0,0 +1,13 @@
+import QtQuick.Controls 2.15
+import QtQuick.Layouts 1.15
+import QtQuick 2.15
+import QLLM 1.0
+
+ApplicationWindow {
+    visible: true
+    width: 1148
+    height: 730
+
+    header: ToolBar {
+    }
+}
